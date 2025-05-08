c272d39a909fbf1456053ed5e2d077dd02376318
MicheleCasani <casanimichele@gmail.com>
2025-05-08 13:37:48 +0200

    [UPD] delete Button.jsx and add LanguagesBtn.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import languagesBtn from './assets/languagesBtn'
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-
+      <languagesBtn />
     </>
   )
 }
